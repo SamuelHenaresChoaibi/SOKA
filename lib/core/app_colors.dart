a6bd298273c6ccb831a370bfa025b29d5db293e3
Wisdom <wisdomimade@paucasesnovescifp.cat>
2026-01-27 09:03:07 +0100

    Add AppColors class for centralized color management

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const primary = Color(0xFFF0F0EF);
+  static const secondary = Color(0xFF363539);
+  static const background = Color(0xFF222222);
+  static const accent = Color.fromARGB(255, 234, 219, 11);
+  static const cursorColor = Color(0xB2FFFFFF);
+}
