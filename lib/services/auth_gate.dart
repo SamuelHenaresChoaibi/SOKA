c9d71e6d4fe182f4db67bc640be01c47cc345e15
Wisdom <wisdomimade@paucasesnovescifp.cat>
2026-01-29 11:27:22 +0100

    Integrate Google Sign-In and Firebase UI OAuth; update AuthGate for sign-in options, modify pubspec.yaml for new dependencies, and adjust GeneratedPluginRegistrant for Google Sign-In plugin

--- lib/services/auth_gate.dart
+++ lib/services/auth_gate.dart
@@ -1,27 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart' as ui_auth;
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+
 import 'package:soka/screens/home_page.dart';
-import 'package:soka/screens/login_screen.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
+
+  static const String googleClientId =
+      '506945347137-bt2cb9i8nfpvg2pne0qbbq7eabkc1085.apps.googleusercontent.com';
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
+        // LOADING
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Scaffold(
             body: Center(child: CircularProgressIndicator()),
           );
         }
 
+        // LOGUEADO
         if (snapshot.hasData) {
           return const HomePage();
         }
 
-        return const LoginScreen();
+        // NO LOGUEADO → LOGIN UI
+        return ui_auth.SignInScreen(
+          providers: [
+            ui_auth.EmailAuthProvider(),
+            GoogleProvider(clientId: googleClientId),
+          ],
+        );
       },
     );
   }
