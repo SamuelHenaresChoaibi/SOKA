0b138a70768ac256f060155b30ba42dc67946829
Samuel Henares Choaibi <samuelhenares@paucasesnovescifp.cat>
2026-02-03 09:31:49 +0100

    Creación de modelos de clases de los objetos de la realtime databases + Muestra de json como ejemplo / plantilla sobre la estructura de datos

---
+++ lib/models/contact_info.dart
@@ -0,0 +1,35 @@
+class ContactInfo {
+  final String adress;
+  final String email;
+  final String instagram;
+  final String phoneNumber;
+  final String website;
+
+  ContactInfo({
+    required this.adress,
+    required this.email,
+    required this.instagram,
+    required this.phoneNumber,
+    required this.website,
+  });
+
+  factory ContactInfo.fromJson(Map<String, dynamic> json) {
+    return ContactInfo(
+      adress: json['adress'],
+      email: json['email'],
+      instagram: json['instagram'],
+      phoneNumber: json['phoneNumber'],
+      website: json['website'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'adress': adress,
+      'email': email,
+      'instagram': instagram,
+      'phoneNumber': phoneNumber,
+      'website': website,
+    };
+  }
+}
