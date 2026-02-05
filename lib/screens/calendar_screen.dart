bd62b87c1402ed222f36eb00d952803be379de3b
Wisdom <wisdomimade@paucasesnovescifp.cat>
2026-02-05 11:59:08 +0100

    Remove body from CalendarScreen widget

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -7,7 +7,7 @@ class CalendarScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Calendar Page')),
-      body: 
+      
     );
   }
 }
