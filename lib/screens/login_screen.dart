c4dc0da1b6decb835c8f903114ed629f04c2aa75
Wisdom <wisdomimade@paucasesnovescifp.cat>
2026-02-12 16:29:02 +0100

    refactoring

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:soka/theme/app_colors.dart';
 import '../services/auth_service.dart';
