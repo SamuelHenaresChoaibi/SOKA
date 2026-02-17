d7bf65fea3eb1e8f207863a1c9f0d895cc4971da
Samuel Henares Choaibi <samuelhenares@paucasesnovescifp.cat>
2026-02-17 09:06:17 +0100

    Update payment_config.dart

--- lib/config/payment_config.dart
+++ lib/config/payment_config.dart
@@ -6,10 +6,10 @@ class PaymentConfig {
       bool.fromEnvironment('PAYPAL_OVERRIDE_INSECURE_CLIENT_CREDENTIALS', defaultValue: false);
 
   static const String paypalClientId =
-      String.fromEnvironment('PAYPAL_CLIENT_ID', defaultValue: 'Ab9IqE_uBKMB7WfDNszWV-AECEb3E17WxKC5Hug_QREtaJYq7kLMOHDiEKooXA0pekncezSSRYhNjsNL');
+      String.fromEnvironment('PAYPAL_CLIENT_ID', defaultValue: '');
 
   static const String paypalSecretKey =
-      String.fromEnvironment('PAYPAL_SECRET_KEY', defaultValue: 'EN_zvYfYTAP1Ezm9MnIlyPKneoRfdL6vwL-vtWKa0f70u6sjR2mazJHQYJ3h5uY2rVj_QBxTRSYhWhJa');
+      String.fromEnvironment('PAYPAL_SECRET_KEY', defaultValue: '');
 
   static bool get isPayPalConfigured =>
       paypalClientId.trim().isNotEmpty && paypalSecretKey.trim().isNotEmpty;
